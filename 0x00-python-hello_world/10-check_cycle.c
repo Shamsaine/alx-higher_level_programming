4eaaf14f04a1ae668220fcdf7c65588c9764a542
Shamsudeen Ibrahim <shamsain3@gmail.com>
2024-01-02 16:44:07 -0800

    added new file

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,16 @@
+#include "list.h"
+
+/**
+ * check_cycle - checks if a linked list contains a cycle innit
+ *
+ * @list: pointer to the head node
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+
+
+	return (0);
+}
