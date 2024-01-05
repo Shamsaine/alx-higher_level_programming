cd48b81943171fdf09dca3d84ec3f6c3f3a18c4b
Shamsudeen Ibrahim <shamsain3@gmail.com>
2024-01-04 16:16:26 -0800

    added a new file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-print(f"{a} + {b} = {add(a, b)}")
+"""Pullingthe total of 1 and 2"""
+
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print(f"{a} + {b} = {add(a, b)}")
