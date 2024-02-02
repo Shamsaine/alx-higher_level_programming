12f5e9acc78e8ae9de409eaf2b6c64f8da221eea
Shamsudeen Ibrahim <shamsain3@gmail.com>
2024-02-02 14:45:00 -0800

    added new file

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
-module
+
+This module is composed by a function that adds two numbers
 
 """
+
 def add_integer(a, b=98):
     """ Function that adds two integer and/or float numbers
 
