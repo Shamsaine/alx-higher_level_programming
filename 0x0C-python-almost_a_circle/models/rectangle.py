c1894958bb326288df610e523fc8f484ed5fdbe7
Shamsudeen Ibrahim <shamsain3@gmail.com>
2024-02-12 18:12:08 -0800

    added new files

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+"""Defines a rectangle class."""
+from models.base import Base
+
 class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         self.__width = width
