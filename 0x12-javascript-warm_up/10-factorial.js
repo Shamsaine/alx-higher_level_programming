c86ca5ab6fd7750d97a2e092d8bcfee070147246
Shamsudeen Ibrahim <shamsain3@gmail.com>
2024-04-10 17:48:35 -0700

    added new files

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,5 +3,5 @@ function fact (n) {
   if (isNaN(n) || n <= 1) return 1;
   return n * fact(n - 1);
 }
-let n = parseInt(process.argv[2]);
+const n = parseInt(process.argv[2]);
 console.log(fact(n));
--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let args = process.argv.slice(2).map((item) => parseInt(item));
+const args = process.argv.slice(2).map((item) => parseInt(item));
 if (args.length <= 1) console.log(0);
 else {
   let max = args[0];
