a8266b7db31951a2c3ef373c62fd6e25c2278ffe
Shamsudeen Ibrahim <shamsain3@gmail.com>
2024-06-19 15:18:49 -0700

    added new files

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
-#!/usr/bin/node
-const a = "C is fun";
-const b = "Python is cool";
-const c = "JavaScript is amazing";
+#!/usr/bin/nodie
+const a = 'C is fun';
+const b = 'Python is cool';
+const c = 'JavaScript is amazing';
 console.log(a);
 console.log(b);
 console.log(c);
