8587b1a775b8d969ce8a14b26673c410c5c9ae9e
Shamsudeen Ibrahim <shamsain3@gmail.com>
2024-07-30 17:53:14 -0700

    added new files

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+const apiUrl = process.argv[2];
+const characterId = '18';
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    films.forEach(film => {
+      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+        count++;
+      }
+    });
+    console.log(count);
+  } else {
+    console.log(`Error: ${response.statusCode}`);
+  }
+});
